e6a66be95fdc1c475861ad84cb4835d6e07ad565
crl03 <clojac03@gmail.com>
2022-11-16 16:01:56 -0500

    Movies entity update and Lombok Dependancy
    Started movies entity and added lombok to the pom file.
    
    Also, flushed cache for the application.properties file.

--- src/main/java/com/example/x3/MovieManagementApp/entities/Movies.java
+++ src/main/java/com/example/x3/MovieManagementApp/entities/Movies.java
@@ -1,4 +1,31 @@
 package com.example.x3.MovieManagementApp.entities;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "movies")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class Movies {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "movie_name")
+    private String movieName;
+
+    @Column(name = "release_year")
+    private int releaseYear;
+
+    @Column(name = "movie_length")
+    private int movieLength;
+
+    @Column(name = "rating")
+    private double rating;
 }
