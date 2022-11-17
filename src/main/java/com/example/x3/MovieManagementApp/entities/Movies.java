e890dd6c2d9581c8f24f8095d636f0441dea6ae0
crl03 <clojac03@gmail.com>
2022-11-17 08:42:56 -0500

    ID Type Change
    Updated id to type long.

--- src/main/java/com/example/x3/MovieManagementApp/entities/Movies.java
+++ src/main/java/com/example/x3/MovieManagementApp/entities/Movies.java
@@ -15,7 +15,7 @@ public class Movies {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private long id;
 
     @Column(name = "movie_name")
     private String movieName;
