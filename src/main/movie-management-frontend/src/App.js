6fd46b464c103803538beec913e34efea30753dc
Cece Curvin <Canese.Curvin@ibm.com>
2022-11-17 13:53:30 -0600

    removing testing signup

--- src/main/movie-management-frontend/src/App.js
+++ src/main/movie-management-frontend/src/App.js
@@ -5,7 +5,7 @@ import Signup from './pages/Signup/Signup';
 function App() {
   return (
     <div className="App">
-      <Signup />
+      <p>add component here to test setup, but remove before push</p>
     </div>
   );
 }
