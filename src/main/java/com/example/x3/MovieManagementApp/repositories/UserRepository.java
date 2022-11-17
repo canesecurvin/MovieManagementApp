48198f29315b3ce3e9ed40492d880569b08d5e72
Felix P <devfep@gmail.com>
2022-11-17 18:45:03 -0500

    Add repo annotation and necessary abstract repo methods

--- src/main/java/com/example/x3/MovieManagementApp/repositories/UserRepository.java
+++ src/main/java/com/example/x3/MovieManagementApp/repositories/UserRepository.java
@@ -1,12 +1,14 @@
 package com.example.x3.MovieManagementApp.repositories;
 
+import com.example.x3.MovieManagementApp.entities.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
-    Optional<UserEntity> findByUsername(String username);
-    Optional<UserEntity> findByEmail(String email);
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
 }
