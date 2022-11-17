f9b9601a090aa112a1234bc12b0ece00414c05ea
Felix P <devfep@gmail.com>
2022-11-17 18:49:47 -0500

    Set up User controller with registration endpoint logic

--- src/main/java/com/example/x3/MovieManagementApp/controllers/UserController.java
+++ src/main/java/com/example/x3/MovieManagementApp/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.example.x3.MovieManagementApp.controllers;
 
+import com.example.x3.MovieManagementApp.dtos.UserDtos.UserSignUpDto;
 import com.example.x3.MovieManagementApp.repositories.UserRepository;
+import com.example.x3.MovieManagementApp.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RequestMapping("/api/v1")
+@RequestMapping("/v1/users")
 @RestController
 public class UserController {
 
@@ -22,7 +24,7 @@ public class UserController {
     public ResponseEntity<?> registerUser(@RequestBody UserSignUpDto userSignUpDto){
 
         //verify username against repo to prevent multiple user registration with same creds
-        if(userRepository.existsByUsername(userSignUpDto.getUsername())){
+        if(userRepository.existsByUsername(userSignUpDto.getDisplayName())){
             return new ResponseEntity<>("That username is taken. Try another.", HttpStatus.BAD_REQUEST);
         }
 
@@ -32,14 +34,16 @@ public class UserController {
         }
 
         //Post-passed checks against repo
-        UserEntity user = new UserEntity();
-        user.setName(UserSignUpDto.getName());
-        user.setUsername(UserSignUpDto.getUsername());
-        user.setEmail(UserSignUpDto.getEmail());
+        User user = new User();
+        user.setFirstName(userSignUpDto.getFirstName());
+        user.setLastName(userSignUpDto.getLastName());
+        user.setDisplayName(userSignUpDto.getDisplayName());
+        user.setEmail(userSignUpDto.getEmail());
         user.setPassword() //Bcrypt encode password before mapping to entity
 
         userRepository.save(user);
 
+        //Can send User obj based on front end specification; HTTP 200
         return new ResponseEntity<>("User registered successfully", HttpStatus.OK);
 
     }
