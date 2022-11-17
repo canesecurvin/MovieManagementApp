035d4ebef84e20d53e42ccea42b724d33887fc9b
NataliiaBor1 <borisenko.nataliia1@gmail.com>
2022-11-17 00:16:22 -0800

    created base for Genres Entity, MovieComments Entity, Ratings Entity created User Entity made joining user for 3 other tables created empty structure for user repository, user controller, user service

---
+++ src/main/java/com/example/x3/MovieManagementApp/entities/MovieComments.java
@@ -0,0 +1,32 @@
+package com.example.x3.MovieManagementApp.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Movie_comments")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MovieComments {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (name = "comment")
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "user_email")
+    private User user;
+
+
+
+
+
+
+}
