b5ff729ce4372f6be62ac04af2f04fe811d1c662
Cece Curvin <Canese.Curvin@ibm.com>
2022-11-18 12:54:37 -0600

    update

--- src/main/movie-management-frontend/src/App.js
+++ src/main/movie-management-frontend/src/App.js
@@ -1,11 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import MovieList from './pages/MovieList/MovieList.js';
 function App() {
   return (
     <div className="App">
       <p>add component here to test setup, but remove before push</p>
-      <MovieList />
     </div>
   );
 }
