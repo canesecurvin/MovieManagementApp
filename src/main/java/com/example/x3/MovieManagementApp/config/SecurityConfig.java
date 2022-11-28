871458cde8a7740c22e6bec70a07bc9b115f6af6
Felix P <devfep@gmail.com>
2022-11-28 09:58:39 -0500

    set up login user endpoint

--- src/main/java/com/example/x3/MovieManagementApp/config/SecurityConfig.java
+++ src/main/java/com/example/x3/MovieManagementApp/config/SecurityConfig.java
@@ -2,11 +2,13 @@ package com.example.x3.MovieManagementApp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -18,4 +20,11 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
 }
--- src/main/java/com/example/x3/MovieManagementApp/controllers/UserController.java
+++ src/main/java/com/example/x3/MovieManagementApp/controllers/UserController.java
@@ -1,11 +1,16 @@
 package com.example.x3.MovieManagementApp.controllers;
 
 
+import com.example.x3.MovieManagementApp.dtos.UserDtos.UserLoginDto;
 import com.example.x3.MovieManagementApp.dtos.UserDtos.UserSignUpDto;
 import com.example.x3.MovieManagementApp.repositories.UserRepository;
 import com.example.x3.MovieManagementApp.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +30,11 @@ public class UserController {
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody UserSignUpDto userSignUpDto){
         return userService.createNewUser(userSignUpDto);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody UserLoginDto userLoginDto){
+        return userService.loginUser(userLoginDto);
     }
 
 }
