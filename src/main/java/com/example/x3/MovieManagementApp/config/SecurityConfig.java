6d85b8a27c617061a6e02869821f7a2b4e3e3c42
Felix P <devfep@gmail.com>
2022-11-28 14:02:54 -0500

    Update SecurityConfig.java

--- src/main/java/com/example/x3/MovieManagementApp/config/SecurityConfig.java
+++ src/main/java/com/example/x3/MovieManagementApp/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +14,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        //set up authenticated and public routes
+
         http.authorizeRequests()
                 .anyRequest().permitAll()
                 .and().formLogin()
@@ -21,9 +26,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return http.build();
     }
 
-    @Override
     @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
         return super.authenticationManagerBean();
     }
 
