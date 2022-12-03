642868d3414a2b5dd0eaf576960fef915150da1f
Cece Curvin <Canese.Curvin@ibm.com>
2022-12-03 15:59:14 -0600

    edit and delete comment
    functionality to edit and update comments

--- src/main/movie-management-frontend/src/services/MovieCommentsService.js
+++ src/main/movie-management-frontend/src/services/MovieCommentsService.js
@@ -9,17 +9,32 @@ class MovieCommentsService{
     }
 
     addMovieComment(comment, timestamp, movieId, userId){
-        const movieData = {
+        const commentData = {
             comment: comment,
             movieId: movieId,
             userId: userId,
             timestamp: timestamp
         }
-        return axios.post(baseUrl, movieData, { headers: authHeader() });
+        return axios.post(baseUrl, commentData, { headers: authHeader() });
     }
 
     getMovieCommentByMovieAndUser(movieId,userId){
         return axios.get(baseUrl + `${movieId}/${userId}`, { headers: authHeader() });
+    }
+
+    updateComment(comId, comment, timestamp, movieId, userId){
+        const commentData = {
+            id: comId,
+            comment: comment,
+            timestamp: timestamp,
+            movieId: movieId, 
+            userId: userId
+        };
+        return axios.put(baseUrl, commentData, { headers: authHeader() });
+    }
+
+    deleteComment(comId){
+        return axios.delete(baseUrl + comId, { headers: authHeader() });
     }
 }
 
