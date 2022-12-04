2d1ff66e41658cdf455d57e935dc4dd32eef8391
Felix P <devfep@gmail.com>
2022-12-03 21:49:26 -0500

    Update Swagger Config to handle JWT

--- src/main/java/com/example/x3/MovieManagementApp/config/SwaggerConfig.java
+++ src/main/java/com/example/x3/MovieManagementApp/config/SwaggerConfig.java
@@ -4,16 +4,45 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public Docket apiDocket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.x3.MovieManagementApp"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiKey apiKey(){
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext(){
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+
+    }
 
     private ApiInfo apiInfo(){
         return new ApiInfo(
@@ -28,14 +57,6 @@ public class SwaggerConfig {
         );
     }
 
-    @Bean
-    public Docket apiDocket(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.x3.MovieManagementApp"))
-                .paths(PathSelectors.any())
-                .build();
-    }
+
 
 }
