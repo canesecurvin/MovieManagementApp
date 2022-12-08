e59fbd5e56bc89658e292fa3860dab7a8f35482e
Felix P <devfep@gmail.com>
2022-12-07 23:52:41 -0500

    User repo save operations BDD tests

--- src/test/java/com/example/x3/MovieManagementApp/repositories/UserRepositoryTests.java
+++ src/test/java/com/example/x3/MovieManagementApp/repositories/UserRepositoryTests.java
@@ -2,23 +2,24 @@ package com.example.x3.MovieManagementApp.repositories;
 
 import com.example.x3.MovieManagementApp.entities.User;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.OPTIONAL;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+import java.util.Optional;
+
 @DataJpaTest
 public class UserRepositoryTests {
-
     @Autowired
     private UserRepository userRepository;
 
-
     @Test
-    @DisplayName("JUnit test for save user operation")
-    public void givenUserObject_whenSave_thenReturnSavedUser(){
-
-        //given - data setup
+    @DisplayName("Test for save user operation")
+    void givenUserObject_whenSave_thenReturnSavedUser(){
         User user = User.builder()
                 .firstName("John")
                 .lastName("Doe")
@@ -27,14 +28,94 @@ public class UserRepositoryTests {
                 .password("1234")
                 .build();
 
-
-        //when - action
         User savedUser = userRepository.save(user);
 
-
-
-        //then - assertion to verify
         assertThat(savedUser).isNotNull();
-        assertThat(savedUser.getEmail()).contains("@"); //change entity primary key to long
+        assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    @DisplayName("Test for find all operation")
+    void givenMultipleUsers_whenFindAll_thenReturnUserList(){
+        User user = User.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .email("jdoe@gmail.com")
+                .displayName("jdoe")
+                .password("1234")
+                .build();
+
+        User user2 = User.builder()
+                .firstName("Jane")
+                .lastName("Doe")
+                .email("janedoe@gmail.com")
+                .displayName("janeDoe")
+                .password("1234")
+                .build();
+
+        userRepository.save(user);
+        userRepository.save(user2);
+
+        List<User> userList = (List<User>) userRepository.findAll();
+
+        assertThat(userList).isNotNull();
+        assertThat(userList.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("Test for get user by email operation")
+    void givenUserObject_whenFindByEmail_thenReturnUserEmployeeObject(){
+        User user = User.builder()
+            .firstName("John")
+            .lastName("Doe")
+            .email("jdoe@gmail.com")
+            .displayName("jdoe")
+            .password("1234")
+            .build();
+
+        userRepository.save(user);
+        User userDB = userRepository.findByEmail(user.getEmail()).get();
+
+         assertThat(userDB).isNotNull();
+         assertThat(userDB.getEmail()).isEqualTo(user.getEmail());
+    }
+
+    @Test
+    @DisplayName("Test for get user by display name operation")
+    void givenUserObject_whenFindByDisplayName_thenReturnUserEmployeeObject(){
+        User user = User.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .email("jdoe@gmail.com")
+                .displayName("jdoe")
+                .password("1234")
+                .build();
+
+        userRepository.save(user);
+        User userDB = userRepository.findByDisplayName(user.getDisplayName()).get();
+
+        assertThat(userDB).isNotNull();
+        assertThat(userDB.getDisplayName()).isEqualTo(user.getDisplayName());
+    }
+
+    @Test
+    @DisplayName("Test for get user by display name or email operation")
+    void givenUserObject_whenFindByDisplayNameOrEmail_thenReturnUserEmployeeObject(){
+        User user = User.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .email("jdoe@gmail.com")
+                .displayName("jdoe")
+                .password("1234")
+                .build();
+
+        User savedUser = userRepository.save(user);
+
+        Optional<User> userDB = userRepository.findByDisplayNameOrEmail(user.getDisplayName(), user.getEmail());
+
+        assertThat(userDB.isPresent()).isTrue();
+        assertThat(userDB.get().getEmail()).isEqualTo(user.getEmail());
+        assertThat(userDB.get().getDisplayName()).isEqualTo(user.getDisplayName());
+        assertThat(userDB.get().getId()).isEqualTo(savedUser.getId());
     }
 }
